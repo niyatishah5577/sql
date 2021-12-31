3d14ac79c27040b94d2fe5652c9a369eb2bc69b6
niyati55 <64007962+niyatishah5577@users.noreply.github.com>
2021-12-31 11:07:29 +0530

    Add files via upload

---
+++ sql-4.sql
@@ -0,0 +1,35 @@
+use niyati12;
+create table worker2(id int not null,
+firstname varchar(30),
+lastname varchar(30),
+department varchar(30),
+salary int(30),
+primary key (id));
+
+insert into worker2 values(111,"raj","patel","it",92929);
+insert into worker2 values(201,"kiran","patel","it",23455);
+insert into worker2 values(3,"shobha","shah","sales",2345);
+insert into worker2 values(14,"mehul","chokshi","accounts",20933);
+insert into worker2 values(51,"somi","thakkar","it",92882);
+insert into worker2 values(699,"kiran","raj","sales",78828);
+insert into worker2 values(199,"ranbir","sheth","sales",78828);
+insert into worker2 values(19,"ram","shah","sales",72228);
+insert into worker2 values(1990,"birma","sahani","it",88218);
+insert into worker2 values(69,"mabir","patel","sales",65828);
+insert into worker2 values(190,"brijesh   ","sahani","it",88118);
+insert into worker2 values(690,"mabir  ","gandhi","sales",65828);
+
+
+select * from worker2;
+
+select replace(firstname,'a','A'),firstname from worker2;
+select firstname,department
+from
+(select *,rank() over(partition by department order by firstname asc) from worker2)
+as tm order by department desc;
+ 
+select firstname,salary,department 
+from
+(select *,rank() over(partition by department order by salary desc) as rn from worker2) as tf 
+where tf.rn=1;
+
